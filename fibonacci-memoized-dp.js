8ffe899d84afe3308bd4c0b1f83f63f91ac6c16a
tYohannes <teklegy@gmail.com>
2022-04-02 21:43:55 +0300

    Refactor code to remove small fibonacci number calls

--- fibonacci-memoized-dp.js
+++ fibonacci-memoized-dp.js
@@ -2,16 +2,11 @@ const memo = {};
 
 const fib = n => {
   if (memo[n]) return memo[n];
-  
   if (n <= 2) return 1;
-
   memo[n] = fib(n-1) + fib(n-2);
 
   return memo[n];
 }
-
-console.log('fib(5): ', fib(5));
-console.log('fib(10): ', fib(10));
 
 console.time('Execution Time');
 console.log('fib(50): ', fib(50));
--- fibonacci-naive-recursion.js
+++ fibonacci-naive-recursion.js
@@ -4,9 +4,6 @@ const fib = n => {
   return fib(n-1) + fib(n-2);
 }
 
-console.log('fib(5): ', fib(5));
-console.log('fib(10): ', fib(10));
-
 console.time('Execution Time');
 console.log('fib(50): ', fib(50));
 console.timeEnd('Execution Time');
